e439955c8818eb656b7cbe6533094a4b28ec7813
Brian DeRenzi <bderenzi@dimagi.com>
2025-05-16 14:27:15 +0100

    date functions don't always work.

--- utils.py
+++ utils.py
@@ -5,7 +5,10 @@ from datetime import datetime
 
 
 def quote_identifiers(query: str, is_postgres: bool = True) -> str:
-    output_lang = "postgres" if is_postgres else "tsql"
+    output_lang = "postgres"
+    if not is_postgres:
+      output_lang = "tsql"
+      query.replace("CURDATE()","GETDATE()")    
     result = sqlglot.transpile(query, identify=True, write=output_lang)[0]
     logging.info("query_transpiled|%s|%s", query, result)
     return result
